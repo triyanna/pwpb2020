511f0b46ea10d2c48b3dad0f1212d28d6d70c620
Triyanna <mrtriyanna@gmail.com>
2020-09-04 19:30:39 +0700

    Tugas 8

---
+++ Tugas 8/ListView/app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    ArrayList<String> list = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.ListView);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, list);
+        listView.setAdapter(arrayAdapter);
+
+        list.add("RPL");
+        list.add("TKJ");
+        list.add("MM");
+        list.add("AVI");
+        list.add("TOI");
+    }
+}
